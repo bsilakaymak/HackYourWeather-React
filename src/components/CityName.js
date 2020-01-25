810c819bc651ca75f073c4167c1b86802eab559a
Sila <basaksilakaymak@gmail.com>
2020-01-25 12:56:08 +0100

    week-2-hw

--- src/components/CityName.js
+++ src/components/CityName.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const CityName = ({city})=>{
-
-return <h1 style={{fontSize: '1.5em'}}>{city.name}, {city.sys.country}</h1>
+const CityName = ({cityWeather})=>{
+  
+         return <h1 style={{fontSize: '1.5em'}}>{cityWeather.name}, {cityWeather.sys.country}</h1>
 
 }
 
--- src/components/Description.js
+++ src/components/Description.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const Description = ({city})=>{
-   return (
+const Description = ({cityWeather})=>{
+ 
+  return (
    <div style={{padding: '0.5em 0'}}> 
-     <p style={{fontWeight:'bold', fontSize:'1em'}}>{city.weather[0].main}</p>
-     <p style= {{fontStyle:'italic'}}>{city.weather[0].description}</p>   
+     <p style={{fontWeight:'bold', fontSize:'1em'}}>{cityWeather.weather[0].main}</p>
+     <p style= {{fontStyle:'italic'}}>{cityWeather.weather[0].description}</p>   
    </div>
   
    )
